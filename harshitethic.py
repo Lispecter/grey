f4fbee75975fb04aa0457104d161b792680ca603
Lispecter <123044371+Lispecter@users.noreply.github.com>
2023-01-19 02:21:25 -0300

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -4,6 +4,20 @@ from datetime import datetime
 import json, os, string, sys, threading, logging, time, re, random
 import openai
 
+
+# main.py
+import os
+import telegram
+
+def webhook(request):
+    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.message.chat.id
+        # Reply with the same message
+        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    return "ok"
+
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
